11628b3f14126a9328bcbcb5154454c1a411cefb
mateorengel <mateorengel@gmail.com>
2024-08-14 21:58:26 -0400

    last class 2

--- src/redux/default/defaultReducers.js
+++ src/redux/default/defaultReducers.js
@@ -1,13 +1,20 @@
-import {SET_NAME_MODULE} from './defaultTypes'
+import { SET_NAME_MODULE } from './defaultTypes';
 
-const initialState={
-    name : '',
-    age: 0,
-
+const initialState = {
+    name: '',
 };
 
-const defaultReducer = (state = initialState, action) =>{
-    
+const defaultReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_NAME_MODULE : {
+            return {
+                ...state,
+                name: action.payload,
+            }
+        }
+        default:
+            return state;
+    }
 };
 
 export default defaultReducer;

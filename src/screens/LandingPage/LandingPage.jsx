29f50bda0693689dcfdd8d29395ae1ce019734e9
mateorengel <mateorengel@gmail.com>
2024-08-25 04:21:38 -0400

    final CSS

--- src/screens/LandingPage/LandingPage.jsx
+++ src/screens/LandingPage/LandingPage.jsx
@@ -3,34 +3,35 @@ import React from 'react';
 const LandingPage = () => {
   return (
     
-    <div style={{ fontFamily: 'Arial, sans-serif', maxWidth: '800px', margin: '0 auto' }}>
-      
-      <div style={{ backgroundColor: '#5BA04C', padding: '20px', textAlign: 'center', color: 'white' }}>
+    <div className="titulo">
+      <div className="div2">
         <h1>Modulo 7: DESARROLLO FRONTEND CON REACTJS</h1>
       </div>
 
-      <div style={{ marginTop: '20px', padding: '20px' }}>
+      <div className="div3">
         <h2>Bienvenido</h2>
-        <div style={{ width: '100%', height: '4px', backgroundColor: '#5BA04C', margin: '8px 0' }}></div>
+        <div className='div4'></div>
         <p>
-          Este módulo se centra en el uso de <strong>React</strong>, incluyendo la creación de <strong>componentes</strong>, el manejo de <strong>hooks</strong>, y el uso de <strong>Redux</strong>.
+          Este módulo se centra en el uso de <strong>React</strong>, 
+          incluyendo la creación de <strong>componentes</strong>, 
+          el manejo de <strong>hooks</strong>, y el uso de <strong>Redux</strong>.
         </p>
       </div>
 
       <div style={{ padding: '20px' }}>
         <h2>Temas Cubiertos</h2>
-        <div style={{ width: '100%', height: '4px', backgroundColor: '#5BA04C', margin: '8px 0' }}></div>
-        <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '10px' }}>
+        <div className='div4'></div>
+        <table className='tabla'>
           <tbody>
             <tr>
-              <td style={{ verticalAlign: 'top', padding: '8px' }}>Componentes funcionales y de clase</td>
-              <td style={{ verticalAlign: 'top', padding: '8px' }}>Uso de <strong>React hooks</strong> como useState y useEffect</td>
-              <td style={{ verticalAlign: 'top', padding: '8px' }}>Creación de <strong>custom hooks</strong> como useForm</td>
-              <td style={{ verticalAlign: 'top', padding: '8px' }}>Gestión de variables de estado con <strong>useState</strong></td>
-              <td style={{ verticalAlign: 'top', padding: '8px' }}>Gestión de estado global con <strong>Redux</strong></td>
-              <td style={{ verticalAlign: 'top', padding: '8px' }}>Integración de <strong>Redux</strong> con <strong>React</strong></td>
-              <td style={{ verticalAlign: 'top', padding: '8px' }}>Manejo de <strong>Formularios</strong> en <strong>React</strong></td>
-              <td style={{ verticalAlign: 'top', padding: '8px' }}>Publicando nuestra <strong>Página</strong> con <strong>GitHub Pages</strong></td>
+              <td >Componentes funcionales y de clase</td>
+              <td >Uso de <strong>React hooks</strong> como useState y useEffect</td>
+              <td>Creación de <strong>custom hooks</strong> como useForm</td>
+              <td>Gestión de variables de estado con <strong>useState</strong></td>
+              <td>Gestión de estado global con <strong>Redux</strong></td>
+              <td>Integración de <strong>Redux</strong> con <strong>React</strong></td>
+              <td>Manejo de <strong>Formularios</strong> en <strong>React</strong></td>
+              <td>Publicando nuestra <strong>Página</strong> con <strong>GitHub Pages</strong></td>
             </tr>
           </tbody>
         </table>
@@ -38,14 +39,14 @@ const LandingPage = () => {
 
       <div style={{ padding: '20px' }}>
         <h2>Recursos Adicionales</h2>
-        <div style={{ width: '100%', height: '4px', backgroundColor: '#5BA04C', margin: '8px 0' }}></div>
+        <div className='div4'></div>
         <p>
           Para profundizar en los temas cubiertos, consulta los siguientes recursos:
         </p>
       </div>
 
-      <div style={{ textAlign: 'center', padding: '20px', color: '#777' }}>
-      <div style={{ width: '100%', height: '1px', backgroundColor: '#5BA04C', margin: '8px 0' }}></div>
+      <div className='div5'>
+      <div className='div6'></div>
         © 2024 Modulo 7. USIP.
       </div>
     </div>
